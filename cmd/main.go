d338ec4e55f6fb0c25b8e469725e9b908c200cd5
Ben Drucker <bvdrucker@gmail.com>
2020-04-22 17:49:19 -0700

    add --init

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	migrate "github.com/bendrucker/terraform-cloud-migrate"
 
@@ -18,7 +20,7 @@ func main() {
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:  "write",
-				Usage: "Writes proposed changes to disk",
+				Usage: "Writes proposed changes to disk. When --init is set, --write is implicitly set.",
 				Value: false,
 			},
 			&cli.StringFlag{
@@ -54,10 +56,23 @@ func main() {
 				Usage: "A directory where other Terraform modules are stored. If set, it will be scanned recursively for terrafor_remote_state references.",
 				Value: "",
 			},
+			&cli.BoolFlag{
+				Name:  "init",
+				Usage: "When set, 'terraform init' will be called before and after updating configuration. After updating the backend configuration, Terraform will prompt to copy state.",
+				Value: false,
+			},
 		},
 		Action: func(c *cli.Context) error {
 			if !c.IsSet("workspace-name") && !c.IsSet("workspace-prefix") {
 				return errors.New("one of --workspace-name or --workspace-prefix must be set")
+			}
+
+			path := c.Args().First()
+
+			if c.Bool("init") {
+				if err := terraformInit(path); err != nil {
+					return fmt.Errorf("failed to init: %v", err)
+				}
 			}
 
 			migration, diags := migrate.New(c.Args().First(), migrate.Config{
@@ -83,7 +98,7 @@ func main() {
 				return diags
 			}
 
-			if c.Bool("write") {
+			if c.Bool("write") || c.Bool("init") {
 				for path, change := range changes {
 					destination := path
 					if change.Rename != "" {
@@ -121,6 +136,15 @@ func main() {
 				}
 			}
 
+			if c.Bool("init") {
+				if err := terraformInit(path); err != nil {
+					return fmt.Errorf("failed to init: %v", err)
+				}
+			}
+
+			fmt.Println("If your workspace is VCS-enabled, commit these changes and push to trigger a run")
+			fmt.Println("If not, you can now call 'terraform plan' and 'terraform apply' locally.")
+
 			return nil
 		},
 	}
@@ -129,4 +153,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func terraformInit(path string) error {
+	cmd := exec.Command("terraform", "init")
+
+	fmt.Fprint(os.Stderr, "Running 'terraform init'")
+
+	cmd.Dir = path
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
 }
